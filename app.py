f72ca781d9c158ce4a6233b77f75ca4d1f7ccc9a
Emil Møller <139823248+molleremil@users.noreply.github.com>
2024-04-25 16:08:47 +0200

    Update and rename mar.py to app.py

--- app.py
+++ app.py
@@ -1,29 +1,35 @@
 import streamlit as st
 import google.generativeai as genai
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Using the GOOGLE_API_KEY environment variable
-genai.configure = os.getenv('GOOGLE_API_KEY')
+genai.configure = os.environ.get('GOOGLE_API_KEY')
+
 
 # UI Config and settings
 st.set_page_config(page_title="Marmar-Gemini", page_icon="💊")
 st.image('img/banner.png', use_column_width=True)
 
+
 def check_drug_interactions(medications, health_history, gender, age, weight, height):
-    model = genai.GenerativeModel('gemini-pro')  
+    model = genai.GenerativeModel('gemini-pro')
     prompt = f"Given the health history: {health_history}, provide a detailed explanation of the potential risks and interactions among these medications: {medications}. Only if any. Focus on any increased risks, specific side effects, and the mechanism by which these interactions might occur, also consider the health history. Ensure the explanation is comprehensive & easy to understand, covering the pharmacological aspects and clinical implications for patients. Then, based on gender: {gender}, age: {age}, weight: {weight}, height: {height}, and health history {health_history}, offer tailored advice."
     response = model.generate_content(prompt)
     return response.text
 
 
-# Streamlit app interface 
+# Streamlit app interface
 st.title('Empowering you with the knowledge and tools to safely manage your medications.')
 st.write('Welcome to Marmar. Check potential drug interactions and get tailored advice. Please provide the details below to get started.')
 
 # Compulsory field for medications and health history
-medications = st.text_area('Enter the names of the medications you are currently taking, separated by commas:', help='Please enter the names of the medications you are currently taking, separated by commas.')
-health_history = st.text_area('Enter your health history or describe any ailments:', help='Please include any chronic conditions, recent illnesses, or relevant health issues.')
+medications = st.text_area('Enter the names of the medications you are currently taking, separated by commas:',
+                           help='Please enter the names of the medications you are currently taking, separated by commas.')
+health_history = st.text_area('Enter your health history or describe any ailments:',
+                              help='Please include any chronic conditions, recent illnesses, or relevant health issues.')
 
 # Optional fields for personalized advice
 options = ['Male', 'Female']
@@ -35,9 +41,8 @@ height = st.text_input('Enter your height in cm (Optional):', '')
 # Button to check interactions
 if st.button('Check Interactions'):
     if medications and health_history:
-        response = check_drug_interactions(medications, health_history, gender, age, weight, height)
+        response = check_drug_interactions(
+            medications, health_history, gender, age, weight, height)
         st.write('**Response:**', response)
     else:
         st.warning('Please enter the required information to continue.')
-
-
